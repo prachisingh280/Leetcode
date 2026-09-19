dcab4d2a94443d1d41a049701137ff18cf0b2812
Prachi Singh <prachisingh.6824@gmail.com>
2026-09-19 22:24:50 +0530

    Time: 0 ms (100%), Space: 12.5 MB (20.35%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0;
-    int j=0;
+        int i=m-1;
+    int j=n-1;
+    int k = m+n-1;
     vector<int>ans;
     
-    while(i<m && j<n)
+    while(i>=0 && j>=0)
     {
         if(nums1[i]<=nums2[j])
         {
-            ans.push_back(nums1[i]);
-            i++;
+            nums1[k] = nums2[j];
+            k--;
+            j--;
         }
         else
         {
-            ans.push_back(nums2[j]);
-            j++;
+            nums1[k] = nums1[i];
+            k--;
+            i--;
         }
     }
     
-    while(i<m)
+    while(i>=0)
     {
-        ans.push_back(nums1[i]);
-        i++;
+        nums1[k] = nums1[i];
+        i--;
+        k--;
     }
     
-    while(j<n)
+    while(j>=0)
     {
-        ans.push_back(nums2[j]);
-        j++;
+        nums1[k] = nums2[j];
+        j--;
+        k--;
     }
     
-    nums1 = ans;
-
-    for(int i=0; i<ans.size(); i++)
+    for(int i=0; i<nums1.size(); i++)
     {
         cout<<nums1[i]<<" ";
     }
