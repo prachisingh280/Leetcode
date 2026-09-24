8df698c8601b608c850d3059e5cd60cc68ab8106
Prachi Singh <prachisingh.6824@gmail.com>
2026-09-24 16:37:19 +0530

    Time: 0 ms (100%), Space: 10.6 MB (43.46%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,53 +1,60 @@
 class Solution {
 public:
-   int possibility(vector<int>&nums, int mid, int k)
+    int answer(vector<int>&nums, int mid)
 {
     int n = nums.size();
     int sum = 0;
     int cnt = 1;
+    int max_sub_sum = 0;
     
     for(int i=0; i<n; i++)
     {
-        if((sum+nums[i])<=mid)
+        if(sum+nums[i]<=mid)
         {
             sum += nums[i];
         }
         else
         {
-            sum = nums[i];
             cnt++;
+            sum = nums[i];
         }
     }
+    
     return cnt;
 }
-
 int splitArray(vector<int>& nums, int k)
 {
     int n = nums.size();
-    int l = *max_element(nums.begin(),nums.end());
+    int low = *max_element(nums.begin(),nums.end());
     int sum = 0;
     int ans = 0;
     
     for(int i=0; i<n; i++)
     {
-        sum = sum + nums[i];
+        sum += nums[i];
     }
-    int h = sum;
     
-    while(l<=h)
+    int high = sum;
+    
+    while(low<=high)
     {
-        int mid = (l+h)/2;
-        int cnt = possibility(nums,mid,k);
-        if(cnt<=k)
+        int mid = (low+high)/2;
+        int cnt = answer(nums,mid);
+        if(cnt==k)
         {
             ans = mid;
-            h = mid-1;
+            high = mid-1;
+        }
+        else if(cnt<k)
+        {
+            high = mid-1;
         }
         else
         {
-            l = mid+1;
+            low = mid+1;
         }
     }
-    return ans;
+    
+    return low;
 }
 };
